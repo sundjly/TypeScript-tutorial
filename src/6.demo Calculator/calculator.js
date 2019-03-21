21a83477948d28c27fbfd1cf0dbef33dbfb73621
sundjly <sundjly@foxmail.com>
2019-03-22 00:55:02 +0800

    refactor  calculator.ts

--- src/6.demo Calculator/calculator.js
+++ src/6.demo Calculator/calculator.js
@@ -1,100 +1,132 @@
-console.log(1111);
-function createButton(text, container, className) {
-    var button = document.createElement('button');
-    button.textContent = text;
-    if (className) {
-        button.className = className;
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+        this.calculatorKeys = [
+            ['Clear', '÷'],
+            ['7', '8', '9', '×'],
+            ['4', '5', '6', '-'],
+            ['1', '2', '3', '+'],
+            ['0', '.', '='],
+        ];
+        this.createContainer();
+        this.createOutput();
+        this.renderButtons();
+        this.bindEvents();
     }
-    container.appendChild(button);
-    return button;
-}
-var container = document.createElement('div');
-container.classList.add('calculator');
-// 声明输出框
-var output = document.createElement('div');
-output.className = 'calculator-output';
-var span = document.createElement('span');
-span.textContent = '0';
-output.appendChild(span);
-container.appendChild(output);
-var calculatorKeys = [
-    ['Clear', '÷'],
-    ['7', '8', '9', '×'],
-    ['4', '5', '6', '-'],
-    ['1', '2', '3', '+'],
-    ['0', '.', '='],
-];
-calculatorKeys.forEach(function (textList) {
-    var div = document.createElement('div');
-    div.classList.add('row');
-    textList.forEach(function (text) {
-        var button = createButton(text, div, "calculator-button text-" + text);
-    });
-    container.appendChild(div);
-});
-var n1;
-var n2;
-var operator;
-// 事件委托
-container.addEventListener('click', function (event) {
-    // console.log(event.target.textContent)  会报错  需要判断
-    if (event.target instanceof HTMLButtonElement) {
-        var button = event.target;
-        var text = button.textContent;
-        if ('0123456789'.indexOf(text) >= 0) {
-            console.log('数字');
-            if (operator) {
-                if (n2) {
-                    n2 = parseInt(n2.toString() + text);
+    Calculator.prototype.createButton = function (text, container, className) {
+        var button = document.createElement('button');
+        button.textContent = text;
+        if (className) {
+            button.className = className;
+        }
+        container.appendChild(button);
+        return button;
+    };
+    Calculator.prototype.createContainer = function () {
+        var container = document.createElement('div');
+        container.classList.add('calculator');
+        document.body.appendChild(container);
+        this.container = container;
+    };
+    Calculator.prototype.createOutput = function () {
+        var output = document.createElement('div');
+        output.className = 'calculator-output';
+        var span = document.createElement('span');
+        span.textContent = '0';
+        output.appendChild(span);
+        this.container.appendChild(output);
+        this.output = output;
+        this.span = span;
+    };
+    Calculator.prototype.bindEvents = function () {
+        var _this = this;
+        this.container.addEventListener('click', function (event) {
+            // console.log(event.target.textContent)  会报错  需要判断
+            if (event.target instanceof HTMLButtonElement) {
+                var button = event.target;
+                var text = button.textContent;
+                // 操作数字
+                if ('0123456789'.indexOf(text) >= 0) {
+                    console.log('数字');
+                    // 更新n2
+                    if (_this.operator) {
+                        if (_this.n2) {
+                            _this.n2 = parseInt(_this.n2.toString() + text);
+                        }
+                        else {
+                            _this.n2 = parseInt(text);
+                        }
+                        _this.span.textContent = _this.n2.toString();
+                    }
+                    else {
+                        // 更新n1
+                        if (_this.n1) {
+                            _this.n1 = parseInt(_this.n1.toString() + text);
+                        }
+                        else {
+                            _this.n1 = parseInt(text);
+                        }
+                        _this.span.textContent = _this.n1.toString();
+                    }
                 }
-                else {
-                    n2 = parseInt(text);
+                else if ('×÷+-'.indexOf(text) >= 0) {
+                    // update 操作符
+                    _this.operator = text;
                 }
-                span.textContent = n2.toString();
+                else if ('='.indexOf(text) >= 0) {
+                    var result = void 0;
+                    switch (_this.operator) {
+                        case '+':
+                            result = _this.n1 + _this.n2;
+                            _this.span.textContent = result.toString();
+                            break;
+                        case '-':
+                            result = _this.n1 - _this.n2;
+                            _this.span.textContent = result.toString();
+                            break;
+                        case '×':
+                            result = _this.n1 * _this.n2;
+                            _this.span.textContent = result.toString();
+                            break;
+                        case '÷':
+                            if (_this.n2) {
+                                result = _this.n1 / _this.n2;
+                                // 四舍五入
+                                return _this.span.textContent = result.toFixed(2);
+                            }
+                            else {
+                                _this.reset();
+                            }
+                            break;
+                        default: break;
+                    }
+                }
+                else if ('Clear'.indexOf(text) >= 0) {
+                    _this.reset();
+                }
             }
             else {
-                if (n1) {
-                    n1 = parseInt(n1.toString() + text);
-                }
-                else {
-                    n1 = parseInt(text);
-                }
-                span.textContent = n1.toString();
+                console.log('不是按钮的东西');
             }
-        }
-        else if ('×÷+-'.indexOf(text) >= 0) {
-            console.log('操作符');
-            operator = text;
-        }
-        else if ('='.indexOf(text) >= 0) {
-            var result = void 0;
-            if (operator === '+') {
-                result = n1 + n2;
-            }
-            else if (operator === '-') {
-                result = n1 - n2;
-            }
-            else if (operator === '×') {
-                result = n1 * n2;
-            }
-            else if (operator === '÷') {
-                if (n2) {
-                    result = n1 / n2;
-                    // 四舍五入
-                    return span.textContent = result.toFixed(2);
-                }
-            }
-            span.textContent = result.toString();
-        }
-        else if ('Clear'.indexOf(text) >= 0) {
-            n1 = null;
-            n2 = null;
-            operator = null;
-            span.textContent = '0';
-        }
-    }
-    else {
-        console.log('不是按钮的东西');
-    }
-});
-document.body.appendChild(container);
+        });
+    };
+    Calculator.prototype.renderButtons = function () {
+        var _this = this;
+        this.calculatorKeys.forEach(function (textList) {
+            var div = document.createElement('div');
+            div.classList.add('row');
+            textList.forEach(function (text) {
+                _this.createButton(text, div, "calculator-button text-" + text);
+            });
+            _this.container.appendChild(div);
+        });
+    };
+    Calculator.prototype.reset = function () {
+        // 重置
+        this.n1 = null;
+        this.n2 = null;
+        this.operator = null;
+        this.span.textContent = '0';
+    };
+    return Calculator;
+}());
+new Calculator();
--- src/6.demo Calculator/calculator.ts
+++ src/6.demo Calculator/calculator.ts
@@ -1,99 +1,129 @@
+class Calculator {
+    public container: HTMLDivElement;
+    private output: HTMLDivElement;
+    private span: HTMLSpanElement;
+    private n1: number;
+    private n2: number;
+    private operator: string;
+    public calculatorKeys: Array<Array<string>> = [
+        ['Clear', '÷'],
+        ['7', '8', '9', '×'],
+        ['4', '5', '6', '-'],
+        ['1', '2', '3', '+'],
+        ['0', '.', '='],
+    ];
 
-console.log(1111)
-function createButton(text: string, container: HTMLDivElement, className: string): HTMLButtonElement {
-    let button: HTMLButtonElement = document.createElement('button');
-    button.textContent = text;
-    if (className) {
-        button.className = className;
+    constructor() {
+        this.createContainer();
+        this.createOutput();
+        this.renderButtons();
+        this.bindEvents();
     }
-    container.appendChild(button);
-    return button;
+
+    createButton(text: string, container: HTMLDivElement, className: string): HTMLButtonElement {
+        let button: HTMLButtonElement = document.createElement('button');
+        button.textContent = text;
+        if (className) {
+            button.className = className;
+        }
+        container.appendChild(button);
+        return button;
+    }
+
+    createContainer() {
+        let container: HTMLDivElement = document.createElement('div');
+        container.classList.add('calculator');
+        document.body.appendChild(container);
+        this.container = container;
+    }
+
+    createOutput() {
+        let output: HTMLDivElement = document.createElement('div');
+        output.className = 'calculator-output';
+        let span: HTMLSpanElement = document.createElement('span');
+        span.textContent = '0';
+        output.appendChild(span);
+        this.container.appendChild(output);
+        this.output = output;
+        this.span = span;
+    }
+
+    bindEvents() {
+        this.container.addEventListener('click', (event) => {
+            // console.log(event.target.textContent)  会报错  需要判断
+            if (event.target instanceof HTMLButtonElement) {
+                let button: HTMLButtonElement = event.target;
+                let text = button.textContent;
+                // 操作数字
+                if ('0123456789'.indexOf(text) >= 0) {
+                    console.log('数字');
+                    // 更新n2
+                    if (this.operator) {
+                        if (this.n2) {
+                            this.n2 = parseInt(this.n2.toString() + text);
+                        } else {
+                            this.n2 = parseInt(text);
+                        }
+                        this.span.textContent = this.n2.toString();
+                    } else {
+                        // 更新n1
+                        if (this.n1) {
+                            this.n1 = parseInt(this.n1.toString() + text);
+                        } else {
+                            this.n1 = parseInt(text);
+                        }
+                        this.span.textContent = this.n1.toString();
+                    }
+                } else if ('×÷+-'.indexOf(text) >= 0) {
+                    // update 操作符
+                    this.operator = text;
+                } else if ('='.indexOf(text) >= 0) {
+                    let result: number
+                    switch (this.operator) {
+                        case '+': result = this.n1 + this.n2;
+                            this.span.textContent = result.toString();
+                            break;
+                        case '-': result = this.n1 - this.n2;
+                            this.span.textContent = result.toString();
+                            break;
+                        case '×': result = this.n1 * this.n2;
+                            this.span.textContent = result.toString();
+                            break;
+                        case '÷':
+                            if (this.n2) {
+                                result = this.n1 / this.n2;
+                                // 四舍五入
+                                return this.span.textContent = result.toFixed(2);
+                            } else { this.reset() }
+                            break;
+                        default: break;
+                    }
+                } else if ('Clear'.indexOf(text) >= 0) {
+                    this.reset();
+                }
+            } else {
+                console.log('不是按钮的东西')
+            }
+        });
+    }
+
+    renderButtons() {
+        this.calculatorKeys.forEach((textList: Array<string>) => {
+            let div = document.createElement('div');
+            div.classList.add('row');
+            textList.forEach((text: string) => {
+                this.createButton(text, div, `calculator-button text-${text}`);
+            });
+
+            this.container.appendChild(div);
+        });
+    }
+
+    reset() {
+        // 重置
+        this.n1 = null; this.n2 = null; this.operator = null;
+        this.span.textContent = '0';
+    }
 }
 
-let container: HTMLDivElement = document.createElement('div');
-container.classList.add('calculator');
-// 声明输出框
-let output: HTMLDivElement = document.createElement('div');
-output.className = 'calculator-output';
-let span: HTMLSpanElement = document.createElement('span');
-span.textContent = '0';
-output.appendChild(span);
-container.appendChild(output);
-
-let calculatorKeys: Array<Array<string>> = [
-    ['Clear', '÷'],
-    ['7', '8', '9', '×'],
-    ['4', '5', '6', '-'],
-    ['1', '2', '3', '+'],
-    ['0', '.', '='],
-];
-
-calculatorKeys.forEach((textList: Array<string>) => {
-    let div = document.createElement('div');
-    div.classList.add('row');
-    textList.forEach((text: string) => {
-        let button: HTMLButtonElement = createButton(text, div, `calculator-button text-${text}`);
-
-    });
-
-    container.appendChild(div);
-});
-let n1: number;
-let n2: number;
-let operator: string;
-
-// 事件委托
-container.addEventListener('click', function (event) {
-    // console.log(event.target.textContent)  会报错  需要判断
-    if (event.target instanceof HTMLButtonElement) {
-        let button: HTMLButtonElement = event.target;
-        let text = button.textContent;
-        if ('0123456789'.indexOf(text) >= 0) {
-            console.log('数字');
-            if (operator) {
-                if (n2) {
-                    n2 = parseInt(n2.toString() + text);
-                } else {
-                    n2 = parseInt(text);
-                }
-                span.textContent = n2.toString();
-            } else {
-                if (n1) {
-                    n1 = parseInt(n1.toString() + text);
-                } else {
-                    n1 = parseInt(text);
-                }
-                span.textContent = n1.toString();
-            }
-        } else if ('×÷+-'.indexOf(text) >= 0) {
-            console.log('操作符')
-            operator = text;
-        } else if ('='.indexOf(text) >= 0) {
-            let result: number
-            if (operator === '+') {
-                result = n1 + n2;
-            }
-            else if (operator === '-') {
-                result = n1 - n2;
-            }
-            else if (operator === '×') {
-                result = n1 * n2;
-            }
-            else if (operator === '÷') {
-                if(n2){
-                    result = n1 / n2;
-                    // 四舍五入
-                    return span.textContent = result.toFixed(2);
-                }
-            }
-            span.textContent = result.toString();
-        } else if ('Clear'.indexOf(text) >= 0) {
-            n1 = null; n2 = null; operator = null;
-            span.textContent = '0';
-        }
-    } else {
-        console.log('不是按钮的东西')
-    }
-});
-
-document.body.appendChild(container);
+new Calculator();
